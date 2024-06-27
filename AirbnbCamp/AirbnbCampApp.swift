1ff800f4a4cb19e5e3252bb9a5260a42cef27ff9
dongganxixi <ruixing.xixi@gmail.com>
2024-06-27 15:30:48 +0800

    Initial Commit

---
+++ AirbnbCamp/AirbnbCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCampApp.swift
+//  AirbnbCamp
+//
+//  Created by 胡瑞兴 on 2024/6/27.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirbnbCamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AirbnbCamp
+//
+//  Created by 胡瑞兴 on 2024/6/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
